ed1d96d1dccb5dd463b71ccd2c7af777264d8541
ChaitanyaSingla <charlieksingla@gmail.com>
2026-01-16 16:35:10 +0530

    Add Kafka Message Headers in Webhook

--- webhook.go
+++ webhook.go
@@ -72,6 +72,9 @@ type Webhook struct {
 
 	// KafkaTopic is the name of the Kafka topic that receives events from CPE for use in Caduceus.
 	KafkaTopic string `json:"kafka_topic,omitempty"`
+
+	// KafkaMessageHeaders is a map of Kafka message headers which will be injected the a message is produced to Kafka
+	KafkaMessageHeaders map[string]string `json:"kafka_message_headers,omitempty"`
 }
 
 // WebhookRegistration is a special struct for unmarshaling a webhook as part of
